ea621440596ee8c2524aefed42ba89d293a57eab
Quayyum-a <quayyumariyo@gmail.com>
2025-05-17 17:00:10 +0100

    Add AuthService for user registration functionality

---
+++ src/service/auth_service.py
@@ -0,0 +1,10 @@
+class AuthService:
+    def __init__(self, user_repository):
+        self.user_repository = user_repository
+
+    def register_user(self, user):
+        existing_user = self.user_repository.find_user_by_email(user.email)
+        if existing_user:
+            raise ValueError("Email already registered")
+        self.user_repository.save_user(user)
+        return True
