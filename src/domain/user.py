b50f1309a205067557c7992bdc2dacb07e095613
Quayyum-a <quayyumariyo@gmail.com>
2025-05-17 17:43:52 +0100

    Add role attribute to User class constructor

--- src/domain/user.py
+++ src/domain/user.py
@@ -1,5 +1,6 @@
 class User:
-    def __init__(self, full_name, email, password):
+    def __init__(self, full_name, email, password, role):
         self.full_name = full_name
         self.email = email
         self.password = password
+        self.role = role
