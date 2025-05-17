b0a644e095806cca4d0a883b44bde6a71ab8ebeb
Quayyum-a <quayyumariyo@gmail.com>
2025-05-17 17:43:28 +0100

    Add role assignment for Student class during initialization

--- src/domain/student.py
+++ src/domain/student.py
@@ -3,5 +3,5 @@ from src.domain.user import User
 
 class Student(User):
     def __init__(self, email, password, name):
-        super().__init__(email, password, name)
+        super().__init__(email, password, name, role = 'student')
         self.enrolled_courses = []
