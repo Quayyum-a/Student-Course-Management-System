f39c3d32ea8a95b1e2af89f5bddeaf89039241e8
Quayyum-a <quayyumariyo@gmail.com>
2025-05-17 17:01:31 +0100

    Add Course class to manage course details and enrollments

---
+++ src/domain/facilitator.py
@@ -0,0 +1,7 @@
+from src.domain.user import User
+
+
+class Facilitator(User):
+    def __init__(self, full_name, email, password):
+        super().__init__(full_name, email, password)
+        self.created_courses = []
