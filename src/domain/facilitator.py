5e816ef3a7ee03936291dda189dac82fdcf2392a
Quayyum-a <quayyumariyo@gmail.com>
2025-05-17 17:41:44 +0100

    Add role parameter to Facilitator initialization

--- src/domain/facilitator.py
+++ src/domain/facilitator.py
@@ -3,5 +3,5 @@ from src.domain.user import User
 
 class Facilitator(User):
     def __init__(self, full_name, email, password):
-        super().__init__(full_name, email, password)
+        super().__init__(full_name, email, password, role='facilitator')
         self.created_courses = []
